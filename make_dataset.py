40077906a1544f90677cbaa18c90d70caa5a2d99
dai-ichiro <d.a.i-ichiro@outlook.jp>
2021-02-25 23:03:59 +0900

    update make_dataset.py

--- make_dataset.py
+++ make_dataset.py
@@ -1,18 +1,21 @@
 import glob
 import os
+import shutil
 import xml.etree.cElementTree as ET
 
 root1 = 'VOCdevkit'
 root2 = 'VOC2012'
+root3 = 'ImageSets'
 
-os.mkdir('Annotations')
-os.mkdir('Main')
+shutil.rmtree(os.path.join(root1, root2, root3, 'Main'))
+os.mkdir(os.path.join(root1, root2, root3, 'Main'))
 
 all_xml_pass = glob.glob(os.path.join(root1, root2, 'Annotations','*.xml'))
-all_xml_filename = [os.path.split(f)[1] for f in all_xml_pass]
+
+train_data = []
 
 for each_xml_pass in all_xml_pass:
-    xml_filename = os.path.split(each_xml_pass)[1]
+    xml_filename = os.path.basename(each_xml_pass)
     
     tree = ET.parse(each_xml_pass)
     root = tree.getroot()
@@ -30,36 +33,35 @@ for each_xml_pass in all_xml_pass:
     
     if len(bndbox_all)>0:
 
+        train_data.append(xml_filename.replace('.xml', ''))
+
         jpg_filename = xml_filename.replace('xml', 'jpg')
+
         new_root = ET.Element('annotation')
         
-        new_filename = ET.SubElement(new_root, 'filename').text = jpg_filename
+        ET.SubElement(new_root, 'filename').text = jpg_filename
 
         Size = ET.SubElement(new_root, 'size')
-        Width = ET.SubElement(Size, 'width').text = root.find('size/width').text
-        Height = ET.SubElement(Size, 'height').text = root.find('size/height').text
-        Depth = ET.SubElement(Size, 'depth').text = root.find('size/depth').text
+        ET.SubElement(Size, 'width').text = root.find('size/width').text
+        ET.SubElement(Size, 'height').text = root.find('size/height').text
+        ET.SubElement(Size, 'depth').text = root.find('size/depth').text
 
         for new_budbox in bndbox_all:
             Object = ET.SubElement(new_root, 'object')
             
-            Name = ET.SubElement(Object, 'name').text = 'head'
-
-            Difficult = ET.SubElement(Object, 'difficult').text = '0'
+            ET.SubElement(Object, 'name').text = 'head'
+            ET.SubElement(Object, 'difficult').text = '0'
 
             Bndbox = ET.SubElement(Object, 'bndbox')
-            Xmin = ET.SubElement(Bndbox, 'xmin').text = new_budbox[0]
-            Ymin = ET.SubElement(Bndbox, 'ymin').text = new_budbox[1]
-            Xmax = ET.SubElement(Bndbox, 'xmax').text = new_budbox[2]
-            Ymax = ET.SubElement(Bndbox, 'ymax').text = new_budbox[3]
+            ET.SubElement(Bndbox, 'xmin').text = new_budbox[0]
+            ET.SubElement(Bndbox, 'ymin').text = new_budbox[1]
+            ET.SubElement(Bndbox, 'xmax').text = new_budbox[2]
+            ET.SubElement(Bndbox, 'ymax').text = new_budbox[3]
 
         new_tree = ET.ElementTree(new_root) 
 
-        new_tree.write(os.path.join('Annotations',xml_filename)) 
-
-new_xml_files = glob.glob('Annotations/*.xml')
-train_data = [os.path.split(f)[1].replace('.xml','') for f in new_xml_files]
+        new_tree.write(os.path.join(root1, root2, 'Annotations', xml_filename)) 
 
 text = "\n".join(train_data)
-with open(os.path.join('Main', 'train.txt'), "w") as f:
+with open(os.path.join(root1, root2, root3, 'Main', 'train.txt'), "w") as f:
     f.write(text)      
