0e443ab783753ea29b8b86c13400da0ad21ea259
dai-ichiro <53703225+dai-ichiro@users.noreply.github.com>
2021-11-24 22:39:49 +0900

    update train_script.py

--- train_script.py
+++ train_script.py
@@ -5,6 +5,10 @@ dataset_train = ObjectDetector.Dataset.from_voc('VOCdevkit/VOC2012', splits='tra
 time_limit = 5*60*60  # 5 hour
 detector = ObjectDetector()
 hyperparameters = {'batch_size':4}
-detector.fit(dataset_train, time_limit=time_limit, num_trials=2, hyperparameters=hyperparameters)
+hyperparameter_tune_kwargs={'num_trials': 2}
+detector.fit(dataset_train,
+            time_limit = time_limit, 
+            hyperparameters = hyperparameters,
+            hyperparameter_tune_kwargs = hyperparameter_tune_kwargs)
 
 detector.save('detector.ag')
